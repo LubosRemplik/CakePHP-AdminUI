ff1da394b10a362f1594124cd1d42dc581c86e77
Lubos Remplik <lubos@on-idle.com>
2017-01-06 11:51:08 +0100

    Do not use ajax yet

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -1,35 +1,9 @@
 $(function() {
     // filters
-    $('.form-filters select')
-      .selectpicker({
-        selectedTextFormat: 'count > 0'
-      })
-      .on('hidden.bs.select', function(e) {
-        $.ajax({
-          type: 'get',
-          url: document.location.href,
-          data: $(".form-filters :input[value!=''][value!='.']").serialize()
-        });
-      });
-    $('.form-filters input')
-      .on('change keyup keypress', function(e) {
-        var keyCode = e.keyCode || e.which;
-        if (keyCode === 13) { 
-          e.preventDefault();
-          $.ajax({
-            type: 'get',
-            url: document.location.href,
-            data: $(".form-filters :input[value!=''][value!='.']").serialize()
-          });
-          return false;
-        }
-      });
-    $('.form-filters a.btn')
-      .on('click', function(e) {
-        e.preventDefault();
-        $.ajax({
-          type: 'get',
-          url: $(this).attr('href')
-        });
-      });
+    $('.form-filters select').selectpicker({
+      selectedTextFormat: 'count > 0'
+    });
+    $('.form-filters select').on('hidden.bs.select', function (e) {
+      $('.form-filters').submit();
+    });
 });
