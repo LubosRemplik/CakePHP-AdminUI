219ea39e4ce431628e0a81d76dbbb4badf78fca6
Lubos Remplik <lubos@on-idle.com>
2017-11-08 13:50:50 +0100

    Submit filter form on input change too

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -3,6 +3,9 @@ $(function() {
     $('.form-filters select').selectpicker({
       selectedTextFormat: 'count > 0'
     });
+    $('.form-filters input').on('change', function(e) {
+      $('.form-filters').submit();
+    });
     $('.form-filters select').on('hidden.bs.select', function (e) {
       $('.form-filters').submit();
     });
